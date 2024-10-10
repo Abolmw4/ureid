c5bd315038ae5a5ea2b7f93cd35be98ea18a111b
abolfazl asghari <a.asghari251@gmail.com>
2024-10-10 22:54:41 +0330

    Chore: Run using terminal command

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from reidentification_service import Ureid, WriteVideo
-from reidentification_util import load_json_file
+from reid_service.reidentification_service import Ureid, WriteVideo
+from reid_service.reidentification_util import load_json_file
 from multiprocessing import Queue
 
 
 def main():
-    config = load_json_file("../configs/config_service.json")
+    config = load_json_file("configs/config_service.json")
     queue = Queue()
 
     ureidProc = Ureid(reid_model_src=config["reid_model"], yolo_model_src=config["yolo_model"], device='cpu', vidoe_or_image_src=config["vidoe_src"], queue=queue)
